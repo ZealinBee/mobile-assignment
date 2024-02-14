d1f375c81d814043f42591c3aef0c9dc273fe083
Zhiyuan Liu <thenukezealot@gmail.com>
2024-02-14 22:44:42 +0200

    exercise 3 week 4

--- app/src/main/java/com/example/myapplication/Daily.kt
+++ app/src/main/java/com/example/myapplication/Daily.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 
 @Composable
@@ -25,6 +29,7 @@ fun Daily(navController: NavHostController) {
         stringResource(R.string.thursday), stringResource(R.string.friday),
         stringResource(R.string.saturday), stringResource(R.string.sunday)
     )
+    val weatherItemList = mutableListOf<WeatherItem>()
 
     Column(
         modifier = Modifier
@@ -35,15 +40,48 @@ fun Daily(navController: NavHostController) {
 
     ) {
         Header()
+
         for (day in daysOfWeek) {
-            Row {
-                Text(text = "$day ", color = Color.Black)
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "28C", color = Color.Black)
-            }
+
+            val weatherItem = WeatherItem(25.55, "$day", 92)
+            weatherItemList.add(weatherItem)
         }
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+            content = {
+                itemsIndexed(weatherItemList) { index, weatherItem ->
+                    WeatherItemRow(weatherItem = weatherItem)
+                }
+            }
+        )
+
+
         Button(onClick = { navController.navigate("greeting") }) {
             Text(text = stringResource(R.string.button))
         }
+        
+
+    }
+}
+
+@Composable
+fun WeatherItemRow(weatherItem: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = weatherItem.description,
+            modifier = Modifier.weight(1f),
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "${weatherItem.temperature}°C",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
---
+++ app/src/main/java/com/example/myapplication/WeatherItem.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+data class WeatherItem(var temperature: Double, var description: String, val humidity: Int)
