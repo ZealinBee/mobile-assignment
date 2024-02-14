2d54fc107ca9988d0879b88c42dff172cb91e132
Zhiyuan Liu <thenukezealot@gmail.com>
2024-02-14 23:04:02 +0200

    added images and displayed them

--- app/src/main/java/com/example/myapplication/Daily.kt
+++ app/src/main/java/com/example/myapplication/Daily.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -17,9 +19,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
+import java.util.Locale
 
 @Composable
 fun Daily(navController: NavHostController) {
@@ -30,7 +34,8 @@ fun Daily(navController: NavHostController) {
         stringResource(R.string.saturday), stringResource(R.string.sunday)
     )
     val weatherItemList = mutableListOf<WeatherItem>()
-
+    val temperatures = listOf(25.55, 21.55, 23.55, 21.50, 23.40, 20.25, 21.50)
+    val descriptions = listOf("Sunny", "Sunny", "Cloudy", "Sunny", "Cloudy", "Sunny", "Cloudy")
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -40,10 +45,11 @@ fun Daily(navController: NavHostController) {
 
     ) {
         Header()
-
-        for (day in daysOfWeek) {
-
-            val weatherItem = WeatherItem(25.55, "$day", 92)
+        for (i in daysOfWeek.indices) {
+            val day = daysOfWeek[i]
+            val temperature = temperatures[i]
+            val description = descriptions[i]
+            val weatherItem = WeatherItem(temperature, description, 92, day)
             weatherItemList.add(weatherItem)
         }
         LazyColumn(
@@ -66,14 +72,28 @@ fun Daily(navController: NavHostController) {
 
 @Composable
 fun WeatherItemRow(weatherItem: WeatherItem) {
+    val painter = painterResource(
+        id =
+        if (weatherItem.description.toLowerCase(Locale.getDefault()) == "sunny") {
+            R.drawable.images
+        } else {
+            R.drawable.cloudy
+        }
+    )
+
     Row(
         modifier = Modifier
             .padding(vertical = 8.dp)
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier.size(50.dp)
+        )
         Text(
-            text = weatherItem.description,
+            text = weatherItem.dayOfWeek,
             modifier = Modifier.weight(1f),
             color = Color.Black,
             fontWeight = FontWeight.Bold
--- app/src/main/java/com/example/myapplication/WeatherItem.kt
+++ app/src/main/java/com/example/myapplication/WeatherItem.kt
@@ -1,3 +1,3 @@
 package com.example.myapplication
 
-data class WeatherItem(var temperature: Double, var description: String, val humidity: Int)
+data class WeatherItem(var temperature: Double, var description: String, val humidity: Int, var dayOfWeek : String)
